6f9b632911271b31c6c5d08c62de9d445c5e7ca2
smallcoderofme <svensama@outlook.com>
2020-05-13 17:04:39 +0800

    binary_to_list

--- src/post_handler.erl
+++ src/post_handler.erl
@@ -7,7 +7,7 @@ init(Req0, State) ->
 	PostId = cowboy_req:binding(post_id, Req0, 666),
 	#{key1 := Key1, key2 := Key2} = cowboy_req:match_qs([key1, key2], Req0),
 	% #{post_id := PostId} = cowboy_req:match_qs([post_id], Req0),
-	io:format("post handler params : ~w, query key1: ~w key2: ~w ~n", [PostId, Key1, Key2]),
+	io:format("post handler params : ~w, query key1: ~w key2: ~w ~n", [PostId, binary_to_list(Key1), binary_to_list(Key2)]),
 	Req = cowboy_req:reply(200,
 		#{<<"content-type">>=><<"text/plain">>},
 		<<"Post View">>,
